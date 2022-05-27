69a325bd91bc9a43f1d21267133657e1464093bf
lihanxi <3138220743@qq.com>
2022-05-27 23:32:42 +0800

    2022052701

--- javas1/hello-app/src/com/li/literal/LiteralDemo.java
+++ javas1/hello-app/src/com/li/literal/LiteralDemo.java
@@ -7,8 +7,10 @@ public class LiteralDemo {
 //        小数
         System.out.println(19.6);
 //        字符，必须用单引号括起来
+        System.out.println(' ');
         System.out.println('a');
 //        字符串，必须用双引号括起来
         System.out.println("askd");
+//        \n 换行 \t tab一个缩进
     }
 }
