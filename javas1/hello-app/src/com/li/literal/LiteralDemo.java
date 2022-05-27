ffb001eb69b392885a1834077a6bc0249c6b4a5f
lihanxi <3138220743@qq.com>
2022-05-27 23:28:26 +0800

    2022052701

---
+++ javas1/hello-app/src/com/li/literal/LiteralDemo.java
@@ -0,0 +1,14 @@
+package com.li.literal;
+
+public class LiteralDemo {
+    public static void main(String[] args) {
+//        整数
+        System.out.println(666);
+//        小数
+        System.out.println(19.6);
+//        字符，必须用单引号括起来
+        System.out.println('a');
+//        字符串，必须用双引号括起来
+        System.out.println("askd");
+    }
+}
---
+++ javas1/hello-app/src/com/li/note/NoteDemo.java
@@ -0,0 +1,14 @@
+package com.li.note;
+
+//import java.util.Arrays;
+
+public class NoteDemo {
+    public static void main(String[] args) {
+        //注释
+        /*
+         一花一世界，一叶一菩提，
+         一二三四五六七八九十
+        */
+        System.out.println("001");
+    }
+}
